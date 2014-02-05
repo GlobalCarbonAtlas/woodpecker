51c5bf05ac1813177da58a60e370eb03d2610896
vmipsl_MAC <vmipsl@ipsl.jussieu.fr>
2014-02-05 15:18:06 +0100

    Examples : clean path

--- examples.php
+++ examples.php
@@ -19,10 +19,10 @@
 
 <div class="page-header title">
     <h1 style="float:left;">Woodpecker
-        <small>D3-based reusable chart library for time series</small>
+        <small>D3-based reusable chart library for chart (time series by default)</small>
     </h1>
     <div id="link">
-        <a target="_blank" href="https://github.com/GlobalCarbonAtlas/woodpecker/tree/master/Woodpecker"><img class="git-icon" src="examples/GitHub-64px.png"></a>
+        <a target="_blank" href="https://github.com/GlobalCarbonAtlas/WoodpeckerObject"><img class="git-icon" src="examples/GitHub-64px.png"></a>
     </div>
     <div class="clearfix"></div>
 </div>
